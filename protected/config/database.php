946beeb56af7dfbf2f22b1814efa6efd168ab48f
juanlabre <33004288+juanlabre@users.noreply.github.com>
2020-11-24 23:14:43 -0500

    first commit

---
+++ protected/config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+// This is the database connection configuration.
+return array(
+	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+	// uncomment the following lines to use a MySQL database
+
+	'connectionString' => 'mysql:host=b0adnsapq8ga45j0lqtn-mysql.services.clever-cloud.com;dbname=b0adnsapq8ga45j0lqtn',
+	'emulatePrepare' => true,
+	'username' => 'umin4pxpxsk4lljv',
+	'password' => 'TvZS5CnFe0tI9KcfCYsl',
+	'charset' => 'utf8',
+);
